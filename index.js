7801fc709501060fa8631e50a382c740e4f12fa5
kwangje <kwangje1@gmail.com>
2015-08-03 19:34:03 +0000

    SignIn Test...

--- index.js
+++ index.js
@@ -1,82 +1,158 @@
-
-
-
 'use strict';
 
 var	Client = require('node-rest-client').Client,
-		deferred = require('deferred'),
-		client = new Client();
+	deferred = require('deferred'),
+	client = new Client();
 
 module.exports = {
-	API: function(apiOpts) {
-		return {
+	Service: function(apiOpts) {
+		var Service = {
+			// ----------------------------------------------------------
+			// OAuth2 singleton object
+			// ----------------------------------------------------------
 			OAuth2: {
+				accessToken: '',
+				expiresIn: '',
+				scope: '',
+				
+				// get new OAuth2 token from COOD service.
+				// new token is going to be used to request of other services.
 				getToken: function(opts) {
-					var args = {
-						data: {
-							grant_type: "client_credentials",
-							client_id: opts.clientId,
-							client_secret: opts.clientSecret
-						},
-						headers:{
-							"Content-Type": "application/json; charset=utf-8"
-						} 
-					};
+					var self = this,	// this is OAuth2
+						args = {
+							data: {
+								grant_type: "client_credentials",
+								client_id: opts.clientId,
+								client_secret: opts.clientSecret
+							},
+							headers:{
+								"Content-Type": "application/json; charset=utf-8"
+							} 
+						}; // args
 
 					var df = deferred();
 
-					client.post(apiOpts.url + '/oauth/token', args,
+					client.post(apiOpts.apiUrl + '/oauth/token', args,
 							function(data, response) {
 								data = JSON.parse(data);
 
 								if (data.error) {
 									df.reject(data);
 								} else {
+									// We need closure to access variables in OAuth2 object
+									self.accessToken = data.access_token;
+									self.expiresIn = data.expires_in;
+									self.scope = data.scope;
+									
 									df.resolve(data, response);
 								}
-							});
+							}); // client.post
 
 					client.on('error',
 							function(err) {
 								// console.log("알수없는 에러발생: " + err.request.apiOpts);
 								df.reject(err);
-							});
+							}); // client.on('error')
 
 					return df.promise;
-				}
-			},
+				} // getToken()
+			}, // OAuth2
 
+			// ----------------------------------------------------------
+			// User singleton object
+			// ----------------------------------------------------------
 			User: {
 				signUp: function(opts) {
-					var args = {
-						data: {
-							user: {
-								email: opts.email,
-								firstname: opts.firstname,
-								lastname: opts.lastname,
-								nickname: opts.nickname,
-								password: opts.password,
-								password_confirmation: opts.passwordConfirmation,
-								deivce: opts.device,
-								invite_id: opts.invite_id || ''
-							}
-						}
-					};
+					var self = this,	// this is User
+						args = {
+							data: JSON.stringify({
+								user: {
+									email: opts.email,
+									firstname: opts.firstname,
+									lastname: opts.lastname,
+									nickname: opts.nickname,
+									password: opts.password,
+									password_confirmation: opts.passwordConfirmation,
+									deivce: opts.device,
+									invite_id: opts.invite_id || ''
+								}
+							}),
+							type: "text",
+							xhrFields: {
+								withCredentials: true
+							},
+							headers:{
+								"Content-Type": "application/json; charset=utf-8",
+								'Authorization': 'Bearer ' + Service.OAuth2.accessToken,
+	                			'Accept': 'application/vnd.coddr.v1'
+	            			}
+						}; // args
 
 					var df = deferred();
 
-					client.post(apiOpts.url + '/users.json', args,
+					client.post(apiOpts.webUrl + '/users.json', args,
 							function(data, response) {
-							});
+								data = JSON.parse(data);
+
+								if (data.success) {
+									df.resolve(data);
+								} else {
+									df.reject(data);
+								}
+							}); // client.post()
 
 					client.on('error',
 							function(err) {
 								df.reject(err);
-							});
+							}); // client.on('error')
 
 					return df.promise;
-				}
-			}
-		}
+				}, // signUp()
+				
+				signIn: function(opts) {
+					var self = this,
+						args = {
+							data: JSON.stringify({
+								api_user: {
+									email: opts.email,
+									password: opts.password,
+									device: opts.device
+								}
+							}),
+							type: "text",
+							xhrFields: {
+								withCredentials: true
+							},
+							headers:{
+								"Content-Type": "application/json; charset=utf-8",
+								'Authorization': 'Bearer ' + Service.OAuth2.accessToken,
+	                			'Accept': 'application/vnd.coddr.v1'
+	            			}
+						}; // args
+					
+					var df = deferred();
+					
+					client.post(apiOpts.apiUrl + '/api/users/sign_in.json', args,
+						function(data, response) {
+						    data = JSON.parse(data);
+
+							if (data.success) {
+								df.resolve(data);
+							} else {
+								df.reject(data);
+							}
+						}); // client.post()
+					
+					client.on('error',
+						function(err) {
+						    df.reject(err);
+						}); // client.on('error')
+						
+					return df.promise;
+				} // signIn
+			} // User
+		}; // Service
+		
+		return Service;
 	}
 };
--- test/index.js
+++ test/index.js
@@ -1,42 +1,88 @@
 'use strict';
 
 var chai = require('chai'),
-		chaiAsPromised = require('chai-as-promised'),
-		API = require('../index').API;
+	chaiAsPromised = require('chai-as-promised'),
+	Service = require('../index').Service,
+	service = new Service({
+		apiUrl: "http://coddr-v1-dev.coddr.biz:3000",
+		webUrl: "http://coddr-v1-dev.coddr.biz:4000"
+	});
 
 chai.use(chaiAsPromised);
 chai.should();
 
-describe("#getToken", function() {
-	it('OAuth2 getToken should be fulfilled', function(done) {
-		var api = new API({
-					url: "http://coddr-v1-dev.coddr.biz:3000",
-					clientId: "f30013fd0985d0e8cb5a64b1b6e0e6075cf8b10811c8ee000fd7b62f3553c193",
-					clientSecret: "f6578d5fb3e1ad28b9e23a85c24e9409ac570c0442f2255146be2e9a0cc2d3aa"
-				}),
-				promise = api.OAuth2.getToken();
+describe("OAuth2.getToken", function() {
+	it('should be fulfilled', function(done) {
+		var promise = service.OAuth2.getToken({
+			clientId: "f30013fd0985d0e8cb5a64b1b6e0e6075cf8b10811c8ee000fd7b62f3553c193",
+			clientSecret: "f6578d5fb3e1ad28b9e23a85c24e9409ac570c0442f2255146be2e9a0cc2d3aa"
+		});
 		
 		promise.should.be.fulfilled.then(function(data, response) {
-			console.log("data.access_token => " + data.access_token);
-			console.log("data.expires_in => " + data.expires_in);
-			console.log("data.scope => " + data.scope);
+			console.log("oauth2.access_token => " + data.access_token);
+			console.log("oauth2.expires_in => " + data.expires_in);
+			console.log("oauth2.scope => " + data.scope);
 		});
 
 		promise.should.be.fulfilled.and.notify(done);
 	});
 
-	it('OAuth2 getToken should be rejected', function(done) {
-		var api = new API({
-					url: "http://coddr-v1-dev.coddr.biz:3000",
-					clientId: "unknown",
-					clientSecret: "unknown"
-				}),
-				promise = api.OAuth2.getToken();
+	it('should be rejected', function(done) {
+		var promise = service.OAuth2.getToken({
+			clientId: "unknown",
+			clientSecret: "unknown"
+		});
 		
 		promise.should.be.rejected.then(function(err) {
-			console.log("error  => " + JSON.stringify(err));
+			console.log("oauth2 getToken error => " + JSON.stringify(err));
 		});
 
 		promise.should.be.rejected.and.notify(done);
+	});
+});
+
+describe("User.signIn", function() {
+	it('should be fulfilled', function(done) {
+		require('getmac').getMac(function(err, macAddress){
+			if (err)  throw err;
+			
+			var promise = service.User.signIn({
+				email: "kwangje2015@gmail.com",
+				password: "Wnl!@#123",
+				device: {
+					key: macAddress,
+					token: '5576d5db636f6441fa0001dd',	// member id
+					platform: 'desktop'					// application platform
+				}
+			});
+			
+			promise.should.be.fulfilled.then(function(data, response) {
+				console.log("user signIn success => " + JSON.stringify(data));
+			});
+	
+			promise.should.be.fulfilled.and.notify(done);
+		});
+	});
+
+	it('should be rejected', function(done) {
+		require('getmac').getMac(function(err, macAddress){
+			if (err)  throw err;
+			
+			var promise = service.User.signIn({
+				email: "kwangje2015@gmail.com",
+				password: "Wnl!@#1231",
+				device: {
+					key: macAddress,
+					token: '5576d5db636f6441fa0001dd',	// member id
+					platform: 'desktop'					// application platform
+				}
+			});
+			
+			promise.should.be.rejected.then(function(err) {
+				console.log("user signIn error => " + JSON.stringify(err));
+			});
+	
+			promise.should.be.rejected.and.notify(done);
+		});
 	});
 });
